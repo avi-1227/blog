a23d8bc287ea023e58c460383f2e4cc43919ed99
avi-1227 <avinaashprasadd@gmail.com>
2024-07-20 14:56:46 +0530

    Add email functionality

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Server configuration
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'avinaashprasadd@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_POST = 587
+# EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
