c4f393afeba0f08114104b9191d9f55b2895259b
avi-1227 <avinaashprasadd@gmail.com>
2024-07-20 14:27:49 +0530

    Create forms.py and create a new view post_share for sending email to user

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 
@@ -11,3 +12,14 @@ def post_list(request):
 def post_detail(request, id, post):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED, slug=post)
     return render(request, 'blogapp/post/detail.html', {'post':post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blogapp/post/share.html', {'post':post, 'form':form})
