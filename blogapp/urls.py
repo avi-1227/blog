1da5703f923c1b96a292e7305c1e9267e3c1d8be
avi-1227 <avinaashprasadd@gmail.com>
2024-07-21 18:14:17 +0530

    create a url for comment view

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:id>/<slug:post>/', views.post_detail, name='post_detail'),
     path('post/<int:post_id>/', views.post_share, name='post_share'),
+    path('post/<int:post_id>/comment', views.post_comment, name='post_comment'),
 ]
